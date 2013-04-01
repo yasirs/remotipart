2f5253b65b9d9cb04cceca51bcbd4e923f4096c9
yasirs <yasir.suhail@gmail.com>
2013-04-01 17:36:38 -0300

    try html_escape

--- lib/remotipart/render_overrides.rb
+++ lib/remotipart/render_overrides.rb
@@ -13,7 +13,7 @@ module Remotipart
     def render_with_remotipart *args
       render_without_remotipart *args
       if remotipart_submitted?
-        response.body = %{<textarea data-type=\"#{content_type}\" response-code=\"#{response.response_code}\">#{escape_once(response.body)}</textarea>}
+        response.body = %{<textarea data-type=\"#{content_type}\" response-code=\"#{response.response_code}\">#{html_escape(response.body)}</textarea>}
         response.content_type = Mime::HTML
       end
       response_body
